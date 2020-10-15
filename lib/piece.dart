91d609faa901f062ce790d6ced8db9c04da1aa05
Oleksandr Vorobyov <its.voby@gmail.com>
2020-10-15 13:13:49 +0300

    Rook painter

--- lib/piece.dart
+++ lib/piece.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chess/pawn_painter.dart';
+import 'package:flutter_chess/rook_painter.dart';
 
 enum PieceColor { black, white }
 enum PieceType { king, queen, rook, bishop, knight, pawn }
@@ -14,102 +16,24 @@ class Piece extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final majorColor = color == PieceColor.white ? Colors.white : Colors.black;
-    // final minorColor = color == PieceColor.white ? Colors.black : Colors.white;
-
     return SizedBox.expand(
       child: Container(
         child: CustomPaint(
-          painter: PawnPainter(color: majorColor),
+          painter: getPainter,
         ),
       ),
     );
   }
-}
 
-class PawnPainter extends CustomPainter {
-  const PawnPainter({this.color});
-  final Color color;
+  CustomPainter get getPainter {
+    final majorColor = color == PieceColor.white ? Colors.white : Colors.black;
+    final minorColor = color == PieceColor.white ? Colors.black : Colors.white;
 
-  final double strokeWidth = 2;
-
-  final double bottomMargin = 0.1;
-
-  final double bottomRadius = 15;
-  final double middleRadius = 8;
-  final double topRadius = 5;
-
-  final double bottomWidth = 0.5;
-  final double middleWidth = 0.25;
-  final double topWidth = 0.15;
-
-  final double bottomHeight = 0.32;
-  final double middleHeight = 0.22;
-  final double topHeight = 0.15;
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final fillPaint = Paint()
-      ..style = PaintingStyle.fill
-      ..color = color;
-    final strokePaint = Paint()
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = strokeWidth
-      ..color = Colors.black;
-
-    final p0 = Offset(
-      size.width / 2,
-      size.height * (1 - bottomMargin),
-    );
-    final p1 = Offset(
-      size.width * ((1 - bottomWidth) / 2),
-      p0.dy,
-    );
-    final p2 = Offset(
-      size.width * ((1 - middleWidth) / 2),
-      p1.dy - (size.height * bottomHeight),
-    );
-    final p3 = Offset(
-      size.width * ((1 - topWidth) / 2),
-      p2.dy - (size.height * middleHeight),
-    );
-    final p4 = Offset(
-      p0.dx,
-      p3.dy - (size.height * topHeight),
-    );
-    final p5 = Offset(
-      p3.dx + (size.width * topWidth),
-      p3.dy,
-    );
-    final p6 = Offset(
-      p2.dx + (size.width * middleWidth),
-      p2.dy,
-    );
-    final p7 = Offset(
-      p1.dx + (size.width * bottomWidth),
-      p1.dy,
-    );
-
-    final path = Path()
-      ..moveTo(p0.dx, p0.dy)
-      // left side
-      ..lineTo(p1.dx, p1.dy)
-      ..arcToPoint(p2, radius: Radius.circular(bottomRadius))
-      ..arcToPoint(p3, radius: Radius.circular(middleRadius))
-      ..arcToPoint(p4, radius: Radius.circular(topRadius))
-      // right side
-      ..arcToPoint(p5, radius: Radius.circular(topRadius))
-      ..arcToPoint(p6, radius: Radius.circular(middleRadius))
-      ..arcToPoint(p7, radius: Radius.circular(bottomRadius))
-      // to start
-      ..lineTo(p0.dx, p0.dy);
-
-    canvas.drawPath(path, fillPaint);
-    canvas.drawPath(path, strokePaint);
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return false;
+    switch (type) {
+      case PieceType.rook:
+        return RookPainter(majorColor: majorColor, minorColor: minorColor);
+      default:
+        return PawnPainter(color: majorColor);
+    }
   }
 }
