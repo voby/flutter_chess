b234c20fdbca8f1fc0871180323c3cbdca114c70
Oleksandr Vorobyov <its.voby@gmail.com>
2020-11-26 13:42:10 +0200

    Board numbers

--- lib/board.dart
+++ lib/board.dart
@@ -179,12 +179,45 @@ class _BoardState extends State<Board> {
         : List.generate(8, (i) => i);
 
     return seed.map((rankIndex) {
+      final size = MediaQuery.of(context).size.width / 8;
       final squareColor =
           fileIndex % 2 == rankIndex % 2 ? Colors.brown : Colors.brown[100];
+      final numberColor =
+          fileIndex % 2 == rankIndex % 2 ? Colors.brown[100] : Colors.brown;
 
-      return Flexible(
-        child: Container(
-          color: squareColor,
+      return Container(
+        color: squareColor,
+        width: size,
+        height: size,
+        child: Stack(
+          children: [
+            if (focusSide == PieceColor.white ? fileIndex == 7 : fileIndex == 0)
+              Positioned(
+                top: 1,
+                right: 1,
+                child: Text(
+                  (rankIndex + 1).toString(),
+                  style: TextStyle(
+                    color: numberColor,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            if (focusSide == PieceColor.white ? rankIndex == 0 : rankIndex == 7)
+              Positioned(
+                bottom: 1,
+                left: 1,
+                child: Text(
+                  File.values[fileIndex].toString().split('.').last,
+                  style: TextStyle(
+                    color: numberColor,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+          ],
         ),
       );
     }).toList();
